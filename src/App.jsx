d155c83812a3e2ee25111f00279f64c41522e5be
Volodymyr Yeromin <volodymyr.yeromin@pearson.com>
2022-05-14 20:10:11 +0300

    HW-1 - fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,35 @@ import React, {useState} from 'react';
 const Header = React.createElement('h1', {}, 'Simple Counter');
 
 class SubtractButton extends React.Component {
-  render() {
-    return <button onClick={this.props.onClick}>-</button>;
-  }
+    render() {
+        return <button onClick={this.props.onClick}>-</button>;
+    }
 }
 
 class AddButton extends React.PureComponent {
-  render() {
-    return <button onClick={this.props.onClick}>+</button>;
-  }
+    render() {
+        return <button onClick={this.props.onClick}>+</button>;
+    }
 }
 
 const Counter = (props) => {
-  return (
-      <span>{props.number}</span>
-  )
-}
+    return (
+        <span>{props.number}</span>
+    );
+};
 
 function App() {
-  const [number, setNumber] = useState(0);
+    const [number, setNumber] = useState(0);
 
-  return (
-    <div className="App">
-      {Header}
-      <SubtractButton onClick={() => setNumber((prev) => prev - 1)} />
-      <Counter number={number} />
-      <AddButton onClick={() => setNumber((prev) => prev + 1)} />
-    </div>
-  );
+    return (
+        <div className="App">
+            <div>Hello world</div>
+            {Header}
+            <SubtractButton onClick={() => setNumber((prev) => prev - 1)}/>
+            <Counter number={number}/>
+            <AddButton onClick={() => setNumber((prev) => prev + 1)}/>
+        </div>
+    );
 }
 
 export default App;
