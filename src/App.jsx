0d7132d83f7ba0269b6c4b6f83db0a5740d254a5
Volodymyr Yeromin <volodymyr.yeromin@pearson.com>
2022-05-14 18:42:39 +0300

    HW-1

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import './App.css';
+import React, {useState} from 'react';
+
+const Header = React.createElement('h1', {}, 'Simple Counter');
+
+class SubtractButton extends React.Component {
+  render() {
+    return <button onClick={this.props.onClick}>-</button>;
+  }
+}
+
+class AddButton extends React.PureComponent {
+  render() {
+    return <button onClick={this.props.onClick}>+</button>;
+  }
+}
+
+const Counter = (props) => {
+  return (
+      <span>{props.number}</span>
+  )
+}
+
+function App() {
+  const [number, setNumber] = useState(0);
+
+  return (
+    <div className="App">
+      {Header}
+      <SubtractButton onClick={() => setNumber((prev) => prev - 1)} />
+      <Counter number={number} />
+      <AddButton onClick={() => setNumber((prev) => prev + 1)} />
+    </div>
+  );
+}
+
+export default App;
